ba16bbf481b4756091d9a188ce7f8c6e21e3aa39
Yuujin <zecosmax@gmail.com>
2025-01-21 14:26:06 +0300

    AssebmlyInfo.cs + props

---
+++ Properties/AssebmlyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("windows7.0")]
+
+[assembly: AssemblyTitle("SDR# Remote Control Common")]
+[assembly: AssemblyProduct("SDR# Remote Control Common Class Library")]
+[assembly: AssemblyCopyright("Copyright © Yuujin Birukofu (R3BAR) et. al. THE DAWN OF TIME -- 2025")]
+[assembly: AssemblyCompany("dev")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
